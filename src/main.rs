f2071f44c4579d1361cb581ff4d4730b6ea50cdf
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-07 20:03:42 +0900

    chore: replace hello world with first window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world_system).run();
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, change_clear_color)
+        .run();
 }
 
-fn hello_world_system() {
-    println!("hello world");
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn change_clear_color(
+    input: Res<Input<KeyCode>>,
+    mut clear_color: ResMut<ClearColor>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        clear_color.0 = Color::PURPLE;
+    }
 }
