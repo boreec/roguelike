fcb1d0e3fc980a52597295b8862cfe2ce43db1fc
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-08 19:52:43 +0900

    chore: display just character sprite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
 
+const SPRITESHEET_COLS: usize = 3;
+const SPRITESHEET_ROWS: usize = 1;
+const SPRITE_TILE_WIDTH: f32 = 64.0;
+const SPRITE_TILE_HEIGHT: f32 = 64.0;
+const SPRITE_IDX_PLAYER: usize = 2;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
@@ -9,20 +15,31 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
     commands.spawn(Camera2dBundle::default());
 
     let texture_atlas = TextureAtlas::from_grid(
         asset_server.load("img/tileset.png"),
-        Vec2::new(192f32, 64f32),
-        3,
-        1,
+        Vec2::new(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT),
+        SPRITESHEET_COLS,
+        SPRITESHEET_ROWS,
         None,
         None,
     );
 
-    commands.spawn(SpriteBundle {
-        texture: texture_atlas.texture.clone(),
+    let atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn(SpriteSheetBundle {
+        transform: Transform {
+            translation: Vec3::new(100f32, 100f32, 0f32),
+            ..Default::default()
+        },
+        sprite: TextureAtlasSprite::new(SPRITE_IDX_PLAYER),
+        texture_atlas: atlas_handle,
         ..Default::default()
     });
 }
