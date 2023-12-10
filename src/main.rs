e645adb73aa4454bf3dec86095c33a4f5a0e4dc4
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-10 18:51:27 +0900

    feat: add Map struct

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,30 @@ const SPRITESHEET_COLS: usize = 3;
 const SPRITESHEET_ROWS: usize = 1;
 const SPRITE_TILE_WIDTH: f32 = 64.0;
 const SPRITE_TILE_HEIGHT: f32 = 64.0;
+
+const SPRITE_IDX_GRASS: usize = 0;
+const SPRITE_IDX_GRASS_WITH_FLOWER: usize = 10;
 const SPRITE_IDX_PLAYER: usize = 2;
 
 const MAP_WIDTH: usize = 10;
 const MAP_HEIGHT: usize = 10;
 
+#[derive(Clone)]
 enum TileType {
     Grass,
 }
 
 #[derive(Component)]
 struct Map {
-    tiles: Vec<TileType>,
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    fn new(width: usize, height: usize) -> Self {
+        return Map {
+            tiles: vec![TileType::Grass; width * height],
+        };
+    }
 }
 
 fn main() {
@@ -51,9 +63,21 @@ fn setup(
             ..Default::default()
         },
         sprite: TextureAtlasSprite::new(SPRITE_IDX_PLAYER),
-        texture_atlas: atlas_handle,
+        texture_atlas: atlas_handle.clone(),
         ..Default::default()
     });
+
+    let map = Map::new(MAP_WIDTH, MAP_HEIGHT);
+    for (i, tileType) in map.tiles.iter().enumerate() {
+        let x = (i % MAP_WIDTH) as f32;
+        let y = (i / MAP_WIDTH) as f32;
+        commands.spawn(SpriteSheetBundle {
+            transform: Transform::from_xyz(x, y, 0f32),
+            sprite: TextureAtlasSprite::new(SPRITE_IDX_GRASS),
+            texture_atlas: atlas_handle.clone(),
+            ..Default::default()
+        });
+    }
 }
 
 fn check_exit_events(
