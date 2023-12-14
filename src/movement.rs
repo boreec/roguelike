5817adc0f484e2e543409b9704216bf56c70e802
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-14 20:37:00 +0900

    test: add unit test for can_move_left

--- src/movement.rs
+++ src/movement.rs
@@ -33,3 +33,15 @@ pub const fn can_move_up(player_position: &MapPosition) -> bool {
 pub const fn can_move_down(player_position: &MapPosition) -> bool {
     player_position.y < MAP_HEIGHT - 1
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const POSITION_TOP_LEFT: MapPosition = MapPosition { x: 0, y: 0 };
+
+    #[test]
+    fn test_can_move_left() {
+        assert!(!can_move_left(&POSITION_TOP_LEFT));
+    }
+}
