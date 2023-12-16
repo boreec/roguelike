dbfc6ddc32ba88d865963e5f25f7699cb199d9fa
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-16 15:47:28 +0900

    test: fix tests for can_move_left

--- src/movement.rs
+++ src/movement.rs
@@ -42,19 +42,19 @@ pub const fn can_move_down(
 mod tests {
     use super::*;
 
-    const _MAP_FIVE_BY_FIVE: MapSize = MapSize::new(5, 5);
-    const POSITION_MIDDLE: MapPosition = MapPosition::new(2, 2);
-    const POSITION_TOP_LEFT: MapPosition = MapPosition::new(0, 0);
-    const POSITION_TOP_RIGHT: MapPosition = MapPosition::new(4, 0);
-    const POSITION_BOTTOM_LEFT: MapPosition = MapPosition::new(0, 0);
-    const POSITION_BOTTOM_RIGHT: MapPosition = MapPosition::new(0, 0);
+    // const _MAP_FIVE_BY_FIVE: MapSize = MapSize::new(5, 5);
+    const POSITION_MIDDLE: MapPosition = MapPosition { x: 2, y: 2 };
+    const POSITION_TOP_LEFT: MapPosition = MapPosition { x: 0, y: 0 };
+    const POSITION_TOP_RIGHT: MapPosition = MapPosition { x: 4, y: 0 };
+    const POSITION_BOTTOM_LEFT: MapPosition = MapPosition { x: 0, y: 4 };
+    const POSITION_BOTTOM_RIGHT: MapPosition = MapPosition { x: 4, y: 4 };
 
     #[test]
     fn test_can_move_left() {
         assert!(!can_move_left(&POSITION_TOP_LEFT));
         assert!(!can_move_left(&POSITION_BOTTOM_LEFT));
-        assert!(can_move_left(&POSITION_TOP_LEFT));
-        assert!(can_move_left(&POSITION_BOTTOM_LEFT));
+        assert!(can_move_left(&POSITION_TOP_RIGHT));
+        assert!(can_move_left(&POSITION_BOTTOM_RIGHT));
         assert!(can_move_left(&POSITION_MIDDLE));
     }
 }
