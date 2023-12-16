b0f19c0038b214099f8e4b4f69eab50535262b46
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-16 19:04:50 +0900

    chore: adapt const for new tileset

--- src/consts.rs
+++ src/consts.rs
@@ -1,14 +1,15 @@
 pub const WINDOW_WITDH: f32 = 1280.0;
 pub const WINDOW_HEIGHT: f32 = 800.0;
 
-pub const SPRITESHEET_COLS: usize = 3;
+pub const SPRITESHEET_COLS: usize = 4;
 pub const SPRITESHEET_ROWS: usize = 1;
 pub const SPRITE_TILE_WIDTH: f32 = 64.0;
 pub const SPRITE_TILE_HEIGHT: f32 = 64.0;
 
-pub const SPRITE_IDX_GRASS_WITH_FLOWER: usize = 0;
-pub const SPRITE_IDX_GRASS: usize = 1;
-pub const SPRITE_IDX_PLAYER: usize = 2;
+pub const SPRITE_IDX_GRASS: usize = 0;
+pub const SPRITE_IDX_GRASS_WITH_FLOWER: usize = 1;
+pub const SPRITE_IDX_GRASS_WITH_STONE: usize = 2;
+pub const SPRITE_IDX_PLAYER: usize = 3;
 
 pub const MAP_WIDTH: usize = 10;
 pub const MAP_HEIGHT: usize = 10;
