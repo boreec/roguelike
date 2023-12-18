14cc0e334bde43681a858ceaeb72f835eb48ebb7
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-18 19:13:42 +0900

    feat: display grid

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,5 @@
+use bevy::prelude::Color;
+
 pub const WINDOW_WITDH: f32 = 1280.0;
 pub const WINDOW_HEIGHT: f32 = 800.0;
 
@@ -15,8 +17,12 @@ pub const MAP_WIDTH: usize = 10;
 pub const MAP_HEIGHT: usize = 10;
 
 pub const Z_INDEX_TILE: f32 = 0.0;
+pub const Z_INDEX_GRID: f32 = 0.5;
 pub const Z_INDEX_PLAYER: f32 = 1.0;
 
 pub const CAMERA_ZOOM_INCREMENT: f32 = 0.5;
 pub const CAMERA_ZOOM_IN_MAX: f32 = 0.1;
 pub const CAMERA_ZOOM_OUT_MAX: f32 = 4.0;
+
+pub const GRID_COLOR: Color = Color::BLACK;
+pub const GRID_LINE_WIDTH: f32 = 2.0;
