33bc78f1114b6c49fd5fc8fbcf7247d5fedbfdc1
Cyprien Borée <cyprien.boree@tuta.io>
2023-12-18 19:20:17 +0900

    chore: create file grid.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod camera;
 mod consts;
+mod grid;
 mod input;
 mod map;
 mod movement;
@@ -11,6 +12,7 @@ use bevy::prelude::*;
 
 use crate::camera::*;
 use crate::consts::*;
+use crate::grid::*;
 use crate::input::*;
 use crate::map::*;
 use crate::player::*;
@@ -157,55 +159,6 @@ pub fn check_camera_zoom(
         }
     }
     projection.scale = log_scale.exp();
-}
-
-pub fn display_grid(
-    mut commands: Commands,
-    query_map: Query<&Map>,
-    input: Res<Input<KeyCode>>,
-) {
-    if !input.just_pressed(KeyCode::G) {
-        return;
-    }
-
-    let map = query_map.single();
-    let mut line_length = map.height as f32 * SPRITE_TILE_HEIGHT;
-    for i in 0..=map.width {
-        let position_anchor = MapPosition { x: i, y: 0 };
-        let (line_x, _) = calculate_sprite_position(&position_anchor);
-        commands.spawn(SpriteBundle {
-            sprite: Sprite {
-                color: GRID_COLOR,
-                custom_size: Some(Vec2::new(GRID_LINE_WIDTH, line_length)),
-                ..default()
-            },
-            transform: Transform::from_xyz(
-                line_x - SPRITE_TILE_WIDTH / 2.0,
-                map.height as f32 * SPRITE_TILE_HEIGHT / -2.,
-                Z_INDEX_GRID,
-            ),
-            ..default()
-        });
-    }
-
-    line_length = map.width as f32 * SPRITE_TILE_WIDTH;
-    for j in 0..=map.height {
-        let position_anchor = MapPosition { x: 0, y: j };
-        let (_, line_y) = calculate_sprite_position(&position_anchor);
-        commands.spawn(SpriteBundle {
-            sprite: Sprite {
-                color: GRID_COLOR,
-                custom_size: Some(Vec2::new(line_length, GRID_LINE_WIDTH)),
-                ..default()
-            },
-            transform: Transform::from_xyz(
-                map.width as f32 * SPRITE_TILE_HEIGHT / 2.,
-                line_y + SPRITE_TILE_HEIGHT - SPRITE_TILE_HEIGHT / 2.,
-                Z_INDEX_GRID,
-            ),
-            ..default()
-        });
-    }
 }
 
 fn update_camera_position(
