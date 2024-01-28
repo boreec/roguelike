f39392aa9130a7e39e7659dd7e896458d1e3d7e7
Cyprien Borée <cyprien.boree@tuta.io>
2024-01-28 19:40:21 +0900

    chore: set tile coordinate font size related to tile width

--- src/debug/constants.rs
+++ src/debug/constants.rs
@@ -1,3 +1,4 @@
+use crate::prelude::*;
 use bevy::prelude::*;
 
 pub const DEBUG_MODE_KEY: KeyCode = KeyCode::G;
@@ -6,7 +7,7 @@ pub const GRID_COLOR: Color = Color::BLACK;
 pub const GRID_LINE_WIDTH: f32 = 2.0;
 
 pub const TILE_COORDINATE_LABEL_FONT_COLOR: Color = Color::BLACK;
-pub const TILE_COORDINATE_LABEL_FONT_SIZE: f32 = 10.0;
+pub const TILE_COORDINATE_LABEL_FONT_SIZE: f32 = SPRITE_TILE_WIDTH * 0.25;
 
 pub const Z_INDEX_GRID_LINES: f32 = 0.5;
 pub const Z_INDEX_TILE_COORDINATES: f32 = 0.5;
