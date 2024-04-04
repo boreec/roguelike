8b3ffcfac4bada8ad0c6b175d1d814496ec48acc
Cyprien Boree <cyprien.boree@tuta.io>
2024-04-04 18:47:20 +0900

    docs: add constant documentation to sprite tile related values

--- src/constants.rs
+++ src/constants.rs
@@ -9,7 +9,10 @@ pub const WINDOW_HEIGHT: f32 = 800.0;
 /// The window's title.
 pub const WINDOW_TITLE: &str = "roguelike";
 
+/// The width in pixels for sprites representing tiles.
 pub const SPRITE_TILE_WIDTH: f32 = 64.0;
+
+/// The height in pixels for sprites representing tiles.
 pub const SPRITE_TILE_HEIGHT: f32 = 64.0;
 
 pub const Z_INDEX_TILE: f32 = 0.0;
