23ab2a8cff15a2b9502b2c72bf9f64516229b47e
Cyprien Boree <cyprien.boree@tuta.io>
2024-04-06 18:53:24 +0900

    chore: make gen_bool as one liner for map initialization

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -30,8 +30,7 @@ fn initialize_map(
     mut game_next_state: ResMut<NextState<GameState>>,
     tileset: Res<TilesetTerrain>,
 ) {
-    let mut rng = rand::thread_rng();
-    let m = if rng.gen_bool(0.5) {
+    let m = if rand::thread_rng().gen_bool(0.5) {
         Map::from((PerlinNoise::new(), MAP_WIDTH, MAP_HEIGHT))
     } else {
         let mut ca = CellularAutomaton::new(MAP_WIDTH, MAP_HEIGHT, 0.5);
