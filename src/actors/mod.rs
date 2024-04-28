426fa8a56f20899af6c57d42d97d38987000cfe1
Cyprien Boree <cyprien.boree@tuta.io>
2024-04-28 14:58:13 +0900

    chore: check the current_map number for actors initialization

--- src/actors/mod.rs
+++ src/actors/mod.rs
@@ -39,21 +39,38 @@ pub fn cleanup_actors(
 /// Initializes all actors for a given map.
 pub fn initialize_actors(
     mut commands: Commands,
-    query_map: Query<&Map>,
+    query_map: Query<(&Map, &MapNumber)>,
     mut query_player_map_position: Query<&mut MapPosition, With<Player>>,
     tileset: Res<TilesetActor>,
     current_map_number: Res<CurrentMapNumber>,
     mut next_game_state: ResMut<NextState<GameState>>,
 ) {
-    let map = query_map.single();
-    initialize_rabbits(&mut commands, map, &tileset, current_map_number.0);
+    let mut current_map = None;
+    for (map, map_number) in &query_map {
+        if map_number.0 == current_map_number.0 {
+            current_map = Some(map);
+        }
+    }
+
+    let current_map = current_map.unwrap();
+    initialize_rabbits(
+        &mut commands,
+        current_map,
+        &tileset,
+        current_map_number.0,
+    );
 
     // initialize the player only if there's no player created
     let player_map_position = query_player_map_position.get_single_mut();
     if player_map_position.is_err() {
-        initialize_player(&mut commands, map, &tileset, current_map_number.0);
+        initialize_player(
+            &mut commands,
+            current_map,
+            &tileset,
+            current_map_number.0,
+        );
     } else {
-        let new_spawn = map.generate_random_spawning_position();
+        let new_spawn = current_map.generate_random_spawning_position();
         *player_map_position.unwrap() = new_spawn;
     }
     next_game_state.set(GameState::PlayerTurn);
