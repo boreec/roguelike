c9f7b31776ee07755ad7dd231df4a8a869f64b37
Cyprien Boree <cyprien.boree@tuta.io>
2024-05-03 12:04:02 +0900

    chore: rename variables

--- src/input.rs
+++ src/input.rs
@@ -6,12 +6,12 @@ use bevy::prelude::*;
 pub fn check_player_directional_input(
     mut next_state: ResMut<NextState<GameState>>,
     mut query_player: Query<&mut MapPosition, With<Player>>,
-    query_other_actors: Query<&MapPosition, (With<Actor>, Without<Player>)>,
+    query_actors: Query<&MapPosition, (With<Actor>, Without<Player>)>,
     query_map: Query<(&Map, &MapNumber)>,
     input: Res<ButtonInput<KeyCode>>,
     current_map_number: Res<CurrentMapNumber>,
 ) {
-    let mut player_position = query_player.single_mut();
+    let mut player_pos = query_player.single_mut();
 
     let map = {
         let mut map_found = None;
@@ -30,34 +30,33 @@ pub fn check_player_directional_input(
         }
     };
 
-    let occupied_positions: Vec<MapPosition> =
-        query_other_actors.iter().cloned().collect();
+    let occupied_pos: Vec<MapPosition> = query_actors.iter().cloned().collect();
 
     if input.any_just_pressed([KeyCode::ArrowRight, KeyCode::KeyD])
-        && can_move_right(&player_position, map, &occupied_positions)
+        && can_move_right(&player_pos, map, &occupied_pos)
     {
-        move_right(&mut player_position);
+        move_right(&mut player_pos);
         next_state.set(GameState::EnemyTurn);
     }
 
     if input.any_just_pressed([KeyCode::ArrowLeft, KeyCode::KeyA])
-        && can_move_left(&player_position, map, &occupied_positions)
+        && can_move_left(&player_pos, map, &occupied_pos)
     {
-        move_left(&mut player_position);
+        move_left(&mut player_pos);
         next_state.set(GameState::EnemyTurn);
     }
 
     if input.any_just_pressed([KeyCode::ArrowUp, KeyCode::KeyW])
-        && can_move_up(&player_position, map, &occupied_positions)
+        && can_move_up(&player_pos, map, &occupied_pos)
     {
-        move_up(&mut player_position);
+        move_up(&mut player_pos);
         next_state.set(GameState::EnemyTurn);
     }
 
     if input.any_just_pressed([KeyCode::ArrowDown, KeyCode::KeyS])
-        && can_move_down(&player_position, map, &occupied_positions)
+        && can_move_down(&player_pos, map, &occupied_pos)
     {
-        move_down(&mut player_position);
+        move_down(&mut player_pos);
         next_state.set(GameState::EnemyTurn);
     }
 }
