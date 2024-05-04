ed4036868c3cfed9abbc932fb9db5e5feac81b28
Cyprien Boree <cyprien.boree@tuta.io>
2024-05-04 16:47:31 +0900

    fix: typo in documentation comment

--- src/map/tile.rs
+++ src/map/tile.rs
@@ -41,7 +41,7 @@ pub struct TileBundle {
 }
 
 impl TileBundle {
-    /// Creates a new entiy using the `TileBundle` struct components.
+    /// Creates a new entity using the `TileBundle` struct components.
     pub fn new(
         map_position: MapPosition,
         map_number: usize,
