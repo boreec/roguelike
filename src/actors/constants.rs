df78f6053c96a6d475f785ab74060409d5f2ec7a
Cyprien Boree <cyprien.boree@tuta.io>
2024-05-05 14:31:14 +0900

    chore: add constant for a blob actor

--- src/actors/constants.rs
+++ src/actors/constants.rs
@@ -5,10 +5,12 @@ pub const Z_INDEX_ACTOR: f32 = 1.0;
 pub const TILESET_ACTOR_ROWS: usize = 1;
 
 /// The number of columns in the actors tileset image.
-pub const TILESET_ACTOR_COLUMNS: usize = 2;
+pub const TILESET_ACTOR_COLUMNS: usize = 3;
 
 /// The index in the tileset image where the player sprite is located.
 pub const TILESET_ACTOR_IDX_PLAYER: usize = 0;
 
 /// The index in the tileset image where the rabbit sprite is located.
 pub const TILESET_ACTOR_IDX_RABBIT: usize = 1;
+
+pub const TILESET_ACTOR_IDX_BLOB: usize = 2;
