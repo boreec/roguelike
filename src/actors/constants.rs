d1f6d3d72fa58b1662a55c5f434f8788a58f031b
Cyprien Boree <cyprien.boree@tuta.io>
2024-05-05 14:28:35 +0900

    docs: add documentation comment to TILESET_ACTOR_ROWS

--- src/actors/constants.rs
+++ src/actors/constants.rs
@@ -1,5 +1,7 @@
 /// The Z value for displaying actor sprites on the screen.
 pub const Z_INDEX_ACTOR: f32 = 1.0;
+
+/// The number of rows in the actors tileset.
 pub const TILESET_ACTOR_ROWS: usize = 1;
 pub const TILESET_ACTOR_COLUMNS: usize = 2;
 pub const TILESET_ACTOR_IDX_PLAYER: usize = 0;
