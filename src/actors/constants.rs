598ea1a97f2efc330823ca7123102fa13408f446
Cyprien Boree <cyprien.boree@tuta.io>
2024-05-05 14:24:35 +0900

    chore: move actor related constants to the actors module

--- src/actors/constants.rs
+++ src/actors/constants.rs
@@ -1,2 +1,6 @@
 /// The Z value for displaying actor sprites on the screen.
 pub const Z_INDEX_ACTOR: f32 = 1.0;
+pub const TILESET_ACTOR_ROWS: usize = 1;
+pub const TILESET_ACTOR_COLUMNS: usize = 2;
+pub const TILESET_ACTOR_IDX_PLAYER: usize = 0;
+pub const TILESET_ACTOR_IDX_RABBIT: usize = 1;
